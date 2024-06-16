4d8886fb4330f09b0d24f708f98a579ac40ec513
arj4ns <arj4ns@gmail.com>
2024-06-15 17:12:24 -0700

    Added a folder for django. I've been thinking about learning it by building a basic project with it. There are alot of companies that seem to prefer  using it over Express. Regardless, I might dedicate another project to that, I'm not sure at the moment. I've also added some routing to the frontend, as well as a grid component.

---
+++ sve-db-frontend/src/components/Grid.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import axios from "axios"
+
+function Grid(){
+    return(
+        <div id="flex-grid">
+            <p>grid loADED</p>
+        </div>
+    )
+}
+
+export default Grid
